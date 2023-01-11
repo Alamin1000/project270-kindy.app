886e06dce41763e8102bcb79797960eb4dab937d
alamin <alaminhosen44000@gmail.com>
2023-01-11 17:18:51 +0600

    complete

--- js/main.js
+++ js/main.js
@@ -16,39 +16,50 @@
   });
   $(document).mouseup(function (e) {
     var container = $(".offmenu");
-
-    // If the target of the click isn't the container
     if (!container.is(e.target) && container.has(e.target).length === 0) {
       $(".offcanvas-menu").removeClass("active");
       $(".offcanvas-overlay").removeClass("active");
     }
   });
 
-  // owl-carousel
-  $(".brand-active").owlCarousel({
-    loop: true,
-    margin: 50,
-    responsiveClass: true,
-    nav: false,
-    navText: [
-      '<span class="fas fa-chevron-left fa-2x"></span>',
-      '<span class="fas fa-chevron-right fa-2x"></span>',
-    ],
-    responsive: {
-      0: {
-        items: 1,
+  function owlInitialize() {
+    let slider = $(".about-grid-slider-active");
+    if ($(window).width() < 767) {
+      slider.addClass("owl-carousel");
+      slider.owlCarousel({
+        loop: true,
+        margin: 19,
+        stagePadding: 0,
+        responsiveClass: true,
+        autoplay: true,
         nav: false,
-      },
-      600: {
-        items: 3,
-        nav: false,
-      },
-      1000: {
-        items: 4,
-        nav: false,
-        loop: false,
-      },
-    },
+        dots: false,
+        navText: [
+          '<span class="fas fa-chevron-left fa-2x"></span>',
+          '<span class="fas fa-chevron-right fa-2x"></span>',
+        ],
+        responsive: {
+          0: {
+            items: 1,
+            stagePadding: 30,
+            autoWidth: true,
+          },
+          575: {
+            items: 2,
+            stagePadding: 0,
+          },
+        },
+      });
+    } else {
+      slider.owlCarousel("destroy");
+      slider.removeClass("owl-carousel");
+    }
+  }
+  $(document).ready(function (e) {
+    owlInitialize();
+  });
+  $(window).resize(function () {
+    owlInitialize();
   });
 })(jQuery);
 
